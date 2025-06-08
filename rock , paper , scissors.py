544bd8f1faf84ba1a728eba6aab737dfd48d5363
Yassen Kamr <yassenkamar663@gmail.com>
2025-06-08 21:20:42 +0300

    Update rock , paper , scissors.py

--- rock , paper , scissors.py
+++ rock , paper , scissors.py
@@ -13,27 +13,32 @@ if x == 'help':
     print('3- Scissors cut paper = scissors win.')
     print('4- Paper covers rock = paper wins.')
     
-    while True:
-        player_choice = input("\nEnter your choice (rock, paper, scissors) or type 'exit' to quit: ").lower()
+    z = input ('if you want to play write (play) or write (exit) to quit')
+    
+    if z == 'play' :
+    
 
-        if player_choice == 'exit' or player_choice == 'quit':
-            print("Thanks for playing, see you next time! 👋")
-            break
+       while True:
+           player_choice = input("\nEnter your choice (rock, paper, scissors) or type 'exit' to quit: ").lower()
 
-        if player_choice in options:
-            ai_choice = random.choice(options)
-            print(f"You chose {player_choice}, computer chose {ai_choice}!")
+           if player_choice == 'exit' or player_choice == 'quit':
+              print("Thanks for playing, see you next time! 👋")
+              break
 
-            if player_choice == ai_choice:
+           if player_choice in options:
+              ai_choice = random.choice(options)
+              print(f"You chose {player_choice}, computer chose {ai_choice}!")
+
+              if player_choice == ai_choice:
                 print("It's a tie!")
-            elif (player_choice == 'rock' and ai_choice == 'scissors') or \
+              elif (player_choice == 'rock' and ai_choice == 'scissors') or \
                  (player_choice == 'scissors' and ai_choice == 'paper') or \
                  (player_choice == 'paper' and ai_choice == 'rock'):
                 print("You win! 🎉")
-            else:
+              else:
                 print("You lost! 😢")
-        else:
-            print("Invalid choice. Please enter rock, paper, or scissors.")
+           else:
+               print("Invalid choice. Please enter rock, paper, or scissors.")
 
 
 elif x == '':
