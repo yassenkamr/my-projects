6dcfc3e13b4c443bb03f9f6abe859230518eead7
Yassen Kamr <yassenkamar663@gmail.com>
2025-06-24 11:31:46 +0300

    Update ishop calculater.py

--- ishop calculater.py
+++ ishop calculater.py
@@ -1,49 +1,88 @@
 print('**** Welcome to iShop Calculator ****')
 
-prices = []  
-items = []
+item_prices = []  
+item_names = []
 
-x = int(input('\nHow many items do you have: '))
+total_items = input('\nHow many items do you have: ')
+
+while not total_items.isdigit():
+    total_items = input('Please enter a valid number: ')
+
+total_items = int(total_items)
+
 
 print("\nLet's count them!")
 
-for i in range(1, x + 1):
-    item = input(f'\nPlease enter the name of item number {i}: ')
-    price = int(input('Please enter the price of the item: '))
-    items.append(item)
-    prices.append(price)
+for i in range(1, total_items + 1):
+    name = input(f'\nEnter name for item #{i}: ')
 
-ask1 = input('\nAre you done with your shopping? (yes/no): ').lower()
+    while not name.replace (' ', '').isalpha():
+        name = input('Please enter a valid name: ')
+    
+    price = int(input('Enter price for this item (EGP): '))
 
-if ask1 == 'yes':
-    ask2 = input('Do you want to calculate your total price? (yes/no): ').lower()
+    while not str(price).isdigit():
+        price = int(input('Please enter a valid price: '))
+    
+    item_names.append(name)
+    item_prices.append(price)
 
-    if ask2 == 'yes':
+done_shopping = input('\nAre you done with your shopping? (yes/no): ').lower()
+
+while done_shopping not in ['yes', 'no']:
+    done_shopping = input('Please enter either "yes" or "no": ').lower()
+
+if done_shopping == 'yes':
+    calculate_total = input('Do you want to calculate your total price? (yes/no): ').lower()
+
+    while calculate_total not in ['yes', 'no']:
+        calculate_total = input('Please enter either "yes" or "no": ').lower()
+
+    if calculate_total == 'yes':
         print('\n==== Your Receipt ====')
-        for i in range(len(items)):
-            print(f'{i+1}. {items[i]}: {prices[i]} EGP')
+        for i in range(len(item_names)):
+            print(f'{i + 1}. {item_names[i]}: {item_prices[i]} EGP')
         print('======================')
-        print(f'Total: {sum(prices)} EGP')
+        print(f'Total: {sum(item_prices)} EGP')
 
 else:
-    ask3 = input('Do you want to add more items? (yes/no): ').lower()
-    if ask3 == 'yes':
-        x2 = int(input('\nHow many more items do you want to add: '))
+    add_more = input('Do you want to add more items? (yes/no): ').lower()
+    while add_more not in ['yes', 'no']:
+        add_more = input('Please enter either "yes" or "no": ').lower()
 
-        for j in range(1, x2 + 1):
-            item2 = input(f'\nPlease enter the name of extra item number {j}: ')
-            price2 = int(input('Please enter the price of the item: '))
-            items.append(item2)
-            prices.append(price2)
+    if add_more == 'yes':
+        extra_items = int(input('\nHow many more items do you want to add: '))
+        while not str(extra_items).isdigit():
+            extra_items = int(input('Please enter a valid number: '))
 
-    ask4 = input('\nNow are you done with your shopping? (yes/no): ').lower()
+        for j in range(1, extra_items + 1):
+            name = input(f'\nEnter name for extra item #{j}: ')
 
-    if ask4 == 'yes':
-        ask5 = input('Do you want to calculate your total price? (yes/no): ').lower()
+            while not name.replace (' ', '').isalpha():
+                name = input('Please enter a valid name: ')
+            
+            price = int(input('Enter price for this item (EGP): '))
 
-        if ask5 == 'yes':
+            while not str(price).isdigit():
+                price = int(input('Please enter a valid price: '))
+            
+            item_names.append(name)
+            item_prices.append(price)
+
+    done_after_add = input('\nNow are you done with your shopping? (yes/no): ').lower()
+
+    while done_after_add not in ['yes', 'no']:
+        done_after_add = input('Please enter either "yes" or "no": ').lower()
+
+    if done_after_add == 'yes':
+        calculate_total = input('Do you want to calculate your total price? (yes/no): ').lower()
+        while calculate_total not in ['yes', 'no']:
+            calculate_total = input('Please enter either "yes" or "no": ').lower()
+
+        if calculate_total == 'yes':
             print('\n==== Your Receipt ====')
-            for i in range(len(items)):
-                print(f'{i+1}. {items[i]}: {prices[i]} EGP')
+            for i in range(len(item_names)):
+                print(f'{i + 1}. {item_names[i]}: {item_prices[i]} EGP')
             print('======================')
-            print(f'Total: {sum(prices)} EGP')
+            print(f'Total: {sum(item_prices)} EGP')
+
